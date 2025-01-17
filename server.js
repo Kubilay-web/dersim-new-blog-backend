1100ddca72eada0f7f9df2c1c332d9499cd84bf4
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-01-17 16:32:24 +0300

    Updated

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import subscribeRoutes from "./routes/subscribeRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 
 dotenv.config();
 
@@ -23,9 +26,49 @@ mongoose
   .then(() => console.log("MongoDB bağlantısı başarılı"))
   .catch((err) => console.log("MongoDB bağlantısı başarısız: ", err));
 
+// Mock Database
+
+let products = [
+  {
+    _id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dqqynqpwp/image/upload/v1733219632/dfbe0tmvhrcrgaczzrhn.jpg",
+  },
+  {
+    _id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dqqynqpwp/image/upload/v1733219631/lqnvvrzkxpoxbnjhcafd.png",
+  },
+  {
+    _id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dqqynqpwp/image/upload/v1733219631/z6szavdiejzso9d9pj2n.jpg",
+  },
+];
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
 // API Routes
 app.use("/api/subscribe", subscribeRoutes);
 app.use("/api", blogRoutes);
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/payments", paymentRoutes);
 
 // Sunucu başlatma
 const PORT = process.env.PORT || 5000;
