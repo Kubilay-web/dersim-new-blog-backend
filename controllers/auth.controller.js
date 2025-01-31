3bd724ab3464e28401cf543a0e24a9e00b0d9cc8
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-01-31 10:17:24 +0300

    Updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -37,35 +37,38 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "Tüm alanlar gerekli"));
   }
 
   try {
+    // E-posta ile kullanıcıyı bul
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, "User not found"));
+      return next(errorHandler(404, "Kullanıcı bulunamadı"));
     }
+
+    // Şifreyi karşılaştır
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(400, "Invalid password"));
+      return next(errorHandler(400, "Geçersiz şifre"));
     }
+
+    // JWT token oluştur
     const token = jwt.sign(
       { id: validUser._id, isAdmin: validUser.isAdmin },
-      process.env.JWT_SECRET
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" } // Token süresi 1 gün
     );
 
-    console.log(token);
-
+    // Kullanıcı şifresini hariç tutarak geri gönder
     const { password: pass, ...rest } = validUser._doc;
 
     res
       .status(200)
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .json(rest);
+      .cookie("access_token", token, { httpOnly: true }) // JWT token'ı cookie olarak ayarla
+      .json(rest); // Kullanıcı bilgilerini döndür
   } catch (error) {
-    next(error);
+    next(error); // Hata durumunda error handler'a yönlendir
   }
 };
 
