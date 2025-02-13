1ec17a807482007a9eeeb1542dca4d3bcea77b62
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-02-13 13:03:03 +0300

    Updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,6 +6,91 @@ export const test = (req, res) => {
   res.json({ message: "API is working!" });
 };
 
+// export const updateUser = async (req, res, next) => {
+//   try {
+//     if (req.user.id !== req.params.userId) {
+//       return next(errorHandler(403, "You are not allowed to update this user"));
+//     }
+
+//     const updates = {};
+
+//     // Username Validation
+//     if (req.body.username) {
+//       if (req.body.username.length < 7 || req.body.username.length > 20) {
+//         return next(
+//           errorHandler(400, "Username must be between 7 and 20 characters")
+//         );
+//       }
+//       if (req.body.username.includes(" ")) {
+//         return next(errorHandler(400, "Username cannot contain spaces"));
+//       }
+//       if (req.body.username !== req.body.username.toLowerCase()) {
+//         return next(errorHandler(400, "Username must be lowercase"));
+//       }
+//       if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+//         return next(
+//           errorHandler(400, "Username can only contain letters and numbers")
+//         );
+//       }
+//       const usernameExists = await User.findOne({
+//         username: req.body.username,
+//       });
+//       if (usernameExists && usernameExists._id.toString() !== req.user.id) {
+//         return next(errorHandler(400, "Username already taken"));
+//       }
+//       updates.username = req.body.username;
+//     }
+
+//     // Email Validation
+//     if (req.body.email) {
+//       const emailExists = await User.findOne({ email: req.body.email });
+//       if (emailExists && emailExists._id.toString() !== req.user.id) {
+//         return next(errorHandler(400, "Email already taken"));
+//       }
+//       updates.email = req.body.email;
+//     }
+
+//     // Profile Picture
+//     if (req.body.profilePicture) {
+//       updates.profilePicture = req.body.profilePicture;
+//     }
+
+//     // Password Validation and Update
+//     if (req.body.password) {
+//       if (req.body.password.length < 6) {
+//         return next(
+//           errorHandler(400, "Password must be at least 6 characters")
+//         );
+//       }
+//       const validUser = await User.findById(req.params.userId);
+//       const validPassword = bcryptjs.compareSync(
+//         req.body.password,
+//         validUser.password
+//       );
+//       if (validPassword) {
+//         return next(
+//           errorHandler(400, "New password cannot be the same as the old one")
+//         );
+//       }
+//       updates.password = bcryptjs.hashSync(req.body.password, 10);
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.params.userId,
+//       { $set: updates },
+//       { new: true }
+//     );
+
+//     const { password, ...rest } = updatedUser._doc;
+
+//     res.status(200).json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// user.controller.js
+
 export const updateUser = async (req, res, next) => {
   try {
     if (req.user.id !== req.params.userId) {
@@ -14,7 +99,7 @@ export const updateUser = async (req, res, next) => {
 
     const updates = {};
 
-    // Username Validation
+    // Kullanıcı adı doğrulama
     if (req.body.username) {
       if (req.body.username.length < 7 || req.body.username.length > 20) {
         return next(
@@ -41,7 +126,7 @@ export const updateUser = async (req, res, next) => {
       updates.username = req.body.username;
     }
 
-    // Email Validation
+    // E-posta doğrulama
     if (req.body.email) {
       const emailExists = await User.findOne({ email: req.body.email });
       if (emailExists && emailExists._id.toString() !== req.user.id) {
@@ -50,12 +135,16 @@ export const updateUser = async (req, res, next) => {
       updates.email = req.body.email;
     }
 
-    // Profile Picture
-    if (req.body.profilePicture) {
-      updates.profilePicture = req.body.profilePicture;
+    // Profil fotoğrafı yükleme
+    if (req.file) {
+      // Fotoğrafın Cloudinary'ye yüklenmesi
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "user_profiles",
+      });
+      updates.profilePicture = result.secure_url; // Cloudinary URL'si
     }
 
-    // Password Validation and Update
+    // Şifre güncelleme
     if (req.body.password) {
       if (req.body.password.length < 6) {
         return next(
@@ -82,10 +171,9 @@ export const updateUser = async (req, res, next) => {
     );
 
     const { password, ...rest } = updatedUser._doc;
-
-    res.status(200).json(rest);
+    res.status(200).json(rest); // Güncellenmiş kullanıcı bilgilerini döndür
   } catch (error) {
-    next(error);
+    next(error); // Hata varsa next ile hata handler'ına yönlendirme
   }
 };
 
---
+++ upload4.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.js";
+
+// Cloudinary Storage yapılandırması
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "user_profiles", // Resimlerin yükleneceği klasör
+    allowed_formats: ["jpg", "jpeg", "png", "webp"], // İzin verilen formatlar
+  },
+});
+
+const upload4 = multer({ storage });
+
+export default upload4;
